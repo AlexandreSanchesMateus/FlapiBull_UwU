e9728fc86836a96bf52e06ffbe56d9f8e4997171
Jujuazer <justinprat07@gmail.com>
2022-09-07 10:20:07 +0200

    Shot
    t'as compris

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class Player : Entity
 {
-    private int floatingspeed = 1;
+    [SerializeField] private int floatingSpeed = 1;
+    [SerializeField] private float reverseShot = -0.5f;
+    private bool canShot = true;
+
+    [SerializeField] private int numberReverseTic = 5;
 
     void Update()
     {
-        Move(new Vector2(Input.GetAxisRaw("Horizontal"), floatingspeed));
+        Move(new Vector2(Input.GetAxisRaw("Horizontal"), floatingSpeed));
+
+        if (Input.GetKeyDown(KeyCode.E) && canShot)
+        {
+            StartCoroutine(Shot());
+        }
+    }
+
+    private IEnumerator Shot ()
+    {
+        for (int i = 0; i < numberReverseTic; i++)
+        {
+            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y + reverseShot);
+            yield return new WaitForSeconds(0.05f);
+        }
+
+        canShot = false;
+        yield return new WaitForSeconds(1);
+        canShot = true;
     }
 }
